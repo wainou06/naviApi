d5292a7c4992e39cd0173e21f3a58e7a2afda1bc
blessserenity <love060214@naver.com>
2025-08-12 15:48:46 +0900

    사용자 정보 조회 info 작성

--- routes/info.js
+++ routes/info.js
@@ -1,11 +1,40 @@
 const express = require('express')
+const User = require('../models/user')
+const { isLoggedIn, isManager } = require('./middlewares')
 const router = express.Router()
 
-router.get('/managerUser', async (req, res, next) => {
+router.get('/managerUser/:page', isLoggedIn, isManager, async (req, res, next) => {
    try {
+      // console.log(req.query.params)
+      const page = parseInt(req.params.page, 10) || 1
+      const limit = 10
+      const offset = (page - 1) * limit
+
+      const count = await User.count()
+
+      const users = await User.findAll({
+         limit,
+         offset,
+         order: [['createdAt', 'DESC']],
+         attributes: ['id', 'name', 'nick', 'email', 'address', 'phone'],
+      })
+
+      const pageCount = Math.ceil(count / limit)
+
+      res.status(200).json({
+         success: true,
+         users,
+         pagination: {
+            totalUsers: count,
+            currentPage: page,
+            pageCount,
+            limit,
+         },
+         message: '전체 유저 리스트를 성공적으로 불러왔습니다.',
+      })
    } catch (error) {
       error.status = 500
-      error.message = '유저 정보를 불러오는 중 오류가 발생했습니다.'
+      error.message = `유저 정보를 불러오는 중 오류가 발생했습니다. ${error}`
       next(error)
    }
 })
