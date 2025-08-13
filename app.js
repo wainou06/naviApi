9dfe8edb8138bcdd68ee220de7b80ffe859836fd
wainou06 <wainou.6@gmail.com>
2025-08-13 18:38:06 +0900

    채팅 구현

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
@@ -7,41 +8,34 @@ const passport = require('passport')
 const path = require('path')
 require('dotenv').config()
 const { swaggerUi, swaggerSpec } = require('./swagger')
-
-//라우터 및 기타 모듈 불러오기
-const indexRouter = require('./routes')
+const passportConfig = require('./passport')
 const { sequelize } = require('./models')
+
+// 라우터 불러오기
+const indexRouter = require('./routes')
 const itemsRouter = require('./routes/items')
 const orderRouter = require('./routes/order')
 const rentalItemsRouter = require('./routes/rentalItems')
 const rentalOrderRouter = require('./routes/rentalOrder')
 const authRouter = require('./routes/auth')
-const passportConfig = require('./passport')
 const keywordRouter = require('./routes/keyword')
 const matchingRouter = require('./routes/matching')
 const priceProposalRouter = require('./routes/priceproposal')
 const infoRouter = require('./routes/info')
 const chatRouter = require('./routes/chat')
 
-// 구글 연동을 위한 passport 연결
-require('./server/passport')
-require('./passport')
-
-// 시퀄라이즈를 사용한 DB연결
+// DB 연결
 sequelize
-   // DB업데이트시 요거 true로 바꿨다가 false로 다시
    .sync({ force: false })
-   .then(() => {
-      console.log('데이터베이스 연결 성공')
-   })
-   .catch((err) => {
-      console.log('데이터베이스 연결 실패:', err)
-   })
+   .then(() => console.log('데이터베이스 연결 성공'))
+   .catch((err) => console.log('데이터베이스 연결 실패:', err))
 
 const app = express()
 passportConfig()
+
 app.set('port', process.env.PORT || 8002)
 
+// CORS
 app.use(
    cors({
       origin: 'http://localhost:5173',
@@ -49,30 +43,33 @@ app.use(
    })
 )
 
+// Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+// Logger & Static
 app.use(morgan('dev'))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
+// Body & Cookie Parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(
-   session({
-      resave: false,
-      saveUninitialized: true,
-      secret: process.env.COOKIE_SECRET,
-      cookie: {
-         httpOnly: true,
-         secure: false,
-         sameSite: 'lax', // cross-site에서 쿠키 전송을 허용
-      },
-   })
-)
 
+// Session
+const sessionMiddleware = session({
+   resave: false,
+   saveUninitialized: true,
+   secret: process.env.COOKIE_SECRET,
+   cookie: { httpOnly: true, secure: false, sameSite: 'lax' },
+})
+app.use(sessionMiddleware)
+app.set('sessionMiddleware', sessionMiddleware) // Socket.io용
+
+// Passport
 app.use(passport.initialize())
 app.use(passport.session())
 
-//라우터 등록
+// 라우터
 app.use('/', indexRouter)
 app.use('/items', itemsRouter)
 app.use('/order', orderRouter)
@@ -85,26 +82,19 @@ app.use('/priceProposal', priceProposalRouter)
 app.use('/info', infoRouter)
 app.use('/chats', chatRouter)
 
-// 사용자 정보 수정
-app.use('/api/auth', authRouter)
-
+// 404
 app.use((req, res, next) => {
    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
    error.status = 404
    next(error)
 })
 
+// 에러 처리
 app.use((err, req, res, next) => {
    console.error(err)
-
    const statusCode = err.status || 500
-   const errorMessage = err.message || '서버 내부 오류'
-
-   res.status(statusCode).json({
-      success: false,
-      message: errorMessage,
-      error: err,
-   })
+   const message = err.message || '서버 내부 오류'
+   res.status(statusCode).json({ success: false, message, error: err })
 })
 
 module.exports = app
--- server/socketServer.js
+++ server/socketServer.js
@@ -1,17 +1,35 @@
-// server/socketServer.js
+// socketServer.js
 const { Server } = require('socket.io')
 
-function setupSocket(server) {
+function setupSocket(server, sessionMiddleware) {
    const io = new Server(server, {
       cors: {
          origin: process.env.FRONTEND_APP_URL || '*',
          methods: ['GET', 'POST'],
+         credentials: true,
       },
+   })
+
+   // Socket.io에서 세션 사용
+   io.use((socket, next) => {
+      sessionMiddleware(socket.request, {}, next)
    })
 
    io.on('connection', (socket) => {
       console.log('A user connected:', socket.id)
 
+      // 채팅방 입장
+      socket.on('joinChat', (chatId) => {
+         socket.join(chatId)
+         console.log(`Socket ${socket.id} joined chat ${chatId}`)
+      })
+
+      // 메시지 전송
+      socket.on('sendMessage', (message) => {
+         io.to(message.chatId).emit('receiveMessage', message)
+      })
+
+      // 제안 수락 이벤트
       socket.on('sendProposalAccepted', (data) => {
          io.to(data.receiverSocketId).emit('receiveProposalAccepted', data)
       })
