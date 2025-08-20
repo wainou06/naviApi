5d55e3df970754ad86ba8d71c2691f2d495daa61
blessserenity <love060214@naver.com>
2025-08-20 11:31:41 +0900

    별점 오류 고침

--- routes/rating.js
+++ routes/rating.js
@@ -12,9 +12,22 @@ router.get('/:id', isLoggedIn, async (req, res, next) => {
          where: { userId: req.params.id, itemId: itemId },
       })
 
+      if (!priceProposal) {
+         res.status(200).json({
+            success: true,
+            priceProposal: null,
+            rating: null,
+         })
+      }
+
+      const rating = await Rating.findOne({
+         where: { priceproposalId: priceProposal.id },
+      })
+
       res.status(200).json({
          success: true,
          priceProposal,
+         rating,
       })
    } catch (error) {
       error.status = 500
@@ -28,8 +41,14 @@ router.post('/', isLoggedIn, async (req, res, next) => {
       const data = req.body.data
 
       const exRating = await Rating.findOne({
-         where: {},
+         where: { priceproposalId: data.orderId },
       })
+
+      if (exRating) {
+         const error = new Error('이미 평가를 남겼습니다.')
+         error.status = 4004
+         return next(error)
+      }
 
       const rating = await Rating.create({
          toUserId: data.toUserId,
@@ -37,8 +56,9 @@ router.post('/', isLoggedIn, async (req, res, next) => {
          rating: data.rating,
          comment: data.comment,
          userId: data.userId,
-         orderId: data.orderId,
+         orderId: null,
          rentalOrderId: data.rentalOrderId,
+         priceproposalId: data.orderId,
       })
 
       res.status(200).json({
